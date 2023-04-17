7372d944aee369745468b3232e46529030f65b25
Vladislav Lekontsev <other2other2@gmail.com>
2023-04-17 22:51:05 +0400

    done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(msg, key) {
+    if (!msg || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = '';
+
+    for (let i = 0, j = 0; i < msg.length; i++) {
+      const char = msg.toUpperCase()[i];
+
+      if (char.match(/[A-Z]/)) {
+        const keyCode = key.toUpperCase()[j % key.length].charCodeAt(0) - 65;
+        const charCode = char.charCodeAt(0) - 65;
+        const encryptedCharCode = (charCode + keyCode) % 26 + 65;
+        const encryptedChar = String.fromCharCode(encryptedCharCode);
+
+        result += encryptedChar;
+
+        j++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMsg, key) {
+    if (!encryptedMsg || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = '';
+
+    for (let i = 0, j = 0; i < encryptedMsg.length; i++) {
+      const char = encryptedMsg.toUpperCase()[i];
+
+      if (char.match(/[A-Z]/)) {
+        const keyCode = key.toUpperCase()[j % key.length].charCodeAt(0) - 65;
+        const charCode = char.charCodeAt(0) - 65;
+        const decryptedCharCode = ((charCode - keyCode + 26) % 26) + 65;
+        const decryptedChar = String.fromCharCode(decryptedCharCode);
+
+        result += decryptedChar;
+
+        j++;
+      } else {
+        result += char;
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
   }
 }
 
